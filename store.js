5bc25fb2feff15c9c4877c0cbfa6e850031f25de
tomlubega <107110662+tomlubega@users.noreply.github.com>
2024-10-31 18:45:11 +0300

    Add files via upload

---
+++ store.js
@@ -0,0 +1,8 @@
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for (var i =0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function() {
+        console.log('clicked')
+    })
+}
